86866e670909f45a4b0a31eb5ee5012894ece8e2
tusharpatwal001 <thinkdifferent782@gmail.com>
2026-08-11 21:12:41 +0530

    logistic regression from scratch

---
+++ ml_from_scratch/train_lr2.py
@@ -0,0 +1,22 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+from LogisticRegression_demo import LogisticRegression
+
+data = datasets.load_breast_cancer()
+X, y = data.data, data.target  # type: ignore
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
+
+clf = LogisticRegression(lr=0.04)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+def accuracy(y_test, y_pred):
+    return np.sum(y_test == y_pred) / len(y_test)
+
+
+print(accuracy(y_test, y_pred))
